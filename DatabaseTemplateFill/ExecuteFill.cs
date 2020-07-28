a4aeed1d09347050b5fd75a6de407c26a756b252
Uchidero <asceso111@gmail.com>
2020-07-28 18:05:23 +0300

    промежуточный коммит

---
+++ DatabaseTemplateFill/ExecuteFill.cs
@@ -0,0 +1,18 @@
+﻿using DatabaseLibrary;
+using System.Data.Entity.Core.Common.CommandTrees;
+
+namespace DatabaseTemplateFill
+{
+    class ExecuteFill
+    {
+        DataContext db;
+        public ExecuteFill()
+        {
+            db = new DataContext();
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
